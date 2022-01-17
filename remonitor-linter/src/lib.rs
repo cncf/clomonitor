64dcd96016b6979bb7c96f249b2f561fb8a480e7
Sergio Castaño Arteaga <tegioz@icloud.com>
2022-01-17 11:40:13 +0100

    Add some more fields to documentation section
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- remonitor-linter/src/lib.rs
+++ remonitor-linter/src/lib.rs
@@ -17,11 +17,14 @@ pub struct Report {
 /// Documentation section of a linter report.
 #[derive(Debug, Serialize)]
 pub struct Documentation {
+    pub adopters: bool,
     pub code_of_conduct: bool,
     pub contributing: bool,
     pub changelog: bool,
     pub governance: bool,
+    pub maintainers: bool,
     pub readme: bool,
+    pub roadmap: bool,
 }
 
 /// License section of a linter report.
@@ -48,6 +51,11 @@ pub struct Security {
 pub fn lint(root: &Path) -> Result<Report, Box<dyn Error>> {
     Ok(Report {
         documentation: Documentation {
+            adopters: check::path_exists(Globs {
+                root,
+                patterns: vec!["adopters*"],
+                case_sensitive: false,
+            })?,
             code_of_conduct: check::path_exists(Globs {
                 root,
                 patterns: vec!["code*of*conduct.md", "docs/code*of*conduct.md"],
@@ -68,10 +76,20 @@ pub fn lint(root: &Path) -> Result<Report, Box<dyn Error>> {
                 patterns: vec!["GOVERNANCE*", "docs/GOVERNANCE*"],
                 case_sensitive: false,
             })?,
+            maintainers: check::path_exists(Globs {
+                root,
+                patterns: vec!["maintainers*"],
+                case_sensitive: false,
+            })?,
             readme: check::path_exists(Globs {
                 root,
                 patterns: vec!["README*"],
                 case_sensitive: true,
+            })?,
+            roadmap: check::path_exists(Globs {
+                root,
+                patterns: vec!["roadmap*"],
+                case_sensitive: false,
             })?,
         },
         license: check::license(Globs {

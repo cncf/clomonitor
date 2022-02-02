ea612d3ed4c711c6979cb51d68d92d0998b57a30
Sergio Castaño Arteaga <tegioz@icloud.com>
2022-02-02 08:48:50 +0100

    Add build images job to ci workflow
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- remonitor-apiserver/src/main.rs
+++ remonitor-apiserver/src/main.rs
@@ -60,7 +60,7 @@ async fn shutdown_signal() {
     let ctrl_c = async {
         signal::ctrl_c()
             .await
-            .expect("failed to install ctrl+c handler");
+            .expect("failed to install ctrl+c signal handler");
     };
 
     #[cfg(unix)]

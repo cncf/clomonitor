cncf/clomonitor

commit 552af367b4b86d862047064ae081b215c80b53e8

Improve license check (#121)
Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

clomonitor-core/src/linter/primary.rs
@@ -82,7 +82,7 @@ pub async fn lint(options: LintOptions<'_>) -> Result<Report, Error> {
 
     Ok(Report {
         documentation,
-        license: lint_license(options.root, &md)?,
+        license: lint_license(options.root, &md, &gh_md)?,
         best_practices,
         security,
         legal,
@@ -209,13 +209,18 @@ async fn lint_documentation(
 }
 
 /// Run license checks and prepare the report's license section.
-fn lint_license(root: &Path, md: &Option<Metadata>) -> Result<License, Error> {
+fn lint_license(root: &Path, md: &Option<Metadata>, gh_md: &Repository) -> Result<License, Error> {
     // SPDX id
-    let spdx_id = check::license::detect(Globs {
+    let mut spdx_id = check::license::detect(Globs {
         root,
         patterns: LICENSE_FILE,
         case_sensitive: true,
     })?;
+    if spdx_id.is_none() {
+        if let Some(license) = &gh_md.license {
+            spdx_id = Some(license.spdx_id.to_owned());
+        }
+    }
 
     // Approved
     let mut approved: Option<bool> = None;

clomonitor-core/src/linter/check/github.rs
@@ -67,7 +67,7 @@ pub(crate) async fn last_pr_has_dco_check(repo_url: &str) -> Result<bool, Error>
                 "https://github.com/{}/{}/pull/{}/checks",
                 &owner, &repo, pr.number
             );
-            content::remote_matches(&checks_url, vec!["DCO", "All commits are signed off"]).await?
+            content::remote_matches(&checks_url, vec!["DCO"]).await?
         }
         None => false,
     })

commit 6c8e3cbca2402b5ec6bc0e7e8ee455d22f3c19f4

Fix regression detecting license
Introduced in #121

Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

clomonitor-core/src/linter/primary.rs
@@ -218,7 +218,9 @@ fn lint_license(root: &Path, md: &Option<Metadata>, gh_md: &Repository) -> Resul
     })?;
     if spdx_id.is_none() {
         if let Some(license) = &gh_md.license {
-            spdx_id = Some(license.spdx_id.to_owned());
+            if license.spdx_id != "NOASSERTION" {
+                spdx_id = Some(license.spdx_id.to_owned());
+            }
         }
     }
 
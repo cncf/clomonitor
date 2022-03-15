1642cdc10e110bfc58a97a49fcf8f546bcfd7901
Cintia Sánchez García <cynthiasg@icloud.com>
2022-03-15 11:15:23 +0100

    Use circular progressbar in global score (#175)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/layout/common/RoundScore.tsx
+++ web/src/layout/common/RoundScore.tsx
@@ -12,10 +12,29 @@ const RoundScore = (props: Props) => {
   return (
     <div
       data-testid="global-score"
-      style={{ borderColor: `var(--rm-${color})` }}
-      className={`d-flex align-items-center justify-content-center rounded-pill ${styles.value} global ${props.className}`}
+      className={`d-flex align-items-center justify-content-center rounded-pill ${styles.score} ${props.className}`}
     >
-      {props.score}
+      <div className={styles.chart}>
+        <svg viewBox="0 0 36 36" className="d-block">
+          <path
+            className={styles.circleBg}
+            d="M18 2.0845
+          a 15.9155 15.9155 0 0 1 0 31.831
+          a 15.9155 15.9155 0 0 1 0 -31.831"
+          />
+          <path
+            className={styles.circle}
+            strokeDasharray={`${props.score}, 100`}
+            style={{ stroke: `var(--rm-${color})` }}
+            d="M18 2.0845
+          a 15.9155 15.9155 0 0 1 0 31.831
+          a 15.9155 15.9155 0 0 1 0 -31.831"
+          />
+          <text x="18" y="23" className={styles.value}>
+            {props.score}
+          </text>
+        </svg>
+      </div>
     </div>
   );
 };

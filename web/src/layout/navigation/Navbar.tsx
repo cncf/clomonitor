a24c2b83c25545824142da9e67715841bcaf9618
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2022-03-23 23:50:30 +0100

    Display settings in navbar in mobile devices
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/layout/navigation/Navbar.tsx
+++ web/src/layout/navigation/Navbar.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import logo from '../../media/clomonitor.svg';
 import ThemeSwitch from '../common/ThemeSwitch';
 import Searchbar from '../navigation/Searchbar';
+import MobileSettings from './MobileSettings';
 import styles from './Navbar.module.css';
 
 interface Props {
@@ -15,15 +16,16 @@ const Navbar = (props: Props) => {
     <nav className={`navbar ${styles.navbar}`}>
       <div className="container-lg">
         <div className="d-flex flex-column flex-md-row align-items-center justify-content-between w-100">
-          <div className="me-0 me-md-4 mt-2 mt-md-0">
-            <div className="d-flex flex-row align-items-start">
+          <div className="me-0 me-md-4 mt-2 mt-md-0 w-100">
+            <div className="d-flex flex-row align-items-start w-100">
               <Link to="/" onClick={() => props.setScrollPosition(0)} className="cursorPointer">
                 <img className={styles.logo} alt="CLOMonitor logo" src={logo} />
               </Link>
               <div className={`ms-1 badge rounded-0 text-uppercase ${styles.badge} alphaBadge`}>Alpha</div>
+              <MobileSettings />
             </div>
           </div>
-          <Searchbar classNameWrapper="my-3" setScrollPosition={props.setScrollPosition} />
+          <Searchbar classNameWrapper="my-3 w-100" setScrollPosition={props.setScrollPosition} />
           <div className={`d-none d-md-flex flex-row align-items-center ms-auto ${styles.searchWrapper}`}>
             <Link
               to="/stats"

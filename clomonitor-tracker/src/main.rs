731dfe4ce1befcf8288843170f723cc3f02b7238
Sergio Castaño Arteaga <tegioz@icloud.com>
2022-05-24 20:03:57 +0200

    Add support for configurable log output format
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- clomonitor-tracker/src/main.rs
+++ clomonitor-tracker/src/main.rs
@@ -5,6 +5,7 @@ use deadpool_postgres::{Config as DbConfig, Runtime};
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
 use postgres_openssl::MakeTlsConnector;
 use std::path::PathBuf;
+use tracing_subscriber::EnvFilter;
 use which::which;
 
 mod repository;
@@ -22,23 +23,27 @@ struct Args {
 async fn main() -> Result<()> {
     let args = Args::parse();
 
-    // Setup logging
-    if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "clomonitor_tracker=debug")
-    }
-    tracing_subscriber::fmt::init();
-
-    // Check if required external tools are available
-    if which("git").is_err() {
-        return Err(format_err!("git not found in PATH"));
-    }
-
     // Setup configuration
     let cfg = Config::builder()
         .set_default("db.dbname", "clomonitor")?
         .set_default("tracker.concurrency", 10)?
         .add_source(File::from(args.config))
         .build()?;
+
+    // Setup logging
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "clomonitor_tracker=debug")
+    }
+    let s = tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env());
+    match cfg.get_string("log.format").as_deref() {
+        Ok("json") => s.json().init(),
+        _ => s.init(),
+    };
+
+    // Check if required external tools are available
+    if which("git").is_err() {
+        return Err(format_err!("git not found in PATH"));
+    }
 
     // Setup database
     let mut builder = SslConnector::builder(SslMethod::tls())?;

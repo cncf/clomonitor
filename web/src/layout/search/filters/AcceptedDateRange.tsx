dd28bd4b302c629ec675730c6d488a75fe92de71
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2022-06-21 12:08:03 +0200

    Use date input for accepted range in mobile version
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/layout/search/filters/AcceptedDateRange.tsx
+++ web/src/layout/search/filters/AcceptedDateRange.tsx
@@ -2,8 +2,9 @@ import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 
 import classNames from 'classnames';
+import { isEmpty } from 'lodash';
 import moment from 'moment';
-import { useEffect, useRef, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { DateRange, Range, RangeKeyDict } from 'react-date-range';
 
 import useOutsideClick from '../../../hooks/useOutsideClick';
@@ -23,24 +24,56 @@ interface AcceptedDate {
 const INITIAL_DATE = '2016-01-01';
 
 const AcceptedDateRange = (props: Props) => {
+  const startDate = moment('2016-01-01').toDate();
+  const endDate = moment().toDate();
+  const END_DATE = moment(endDate).format('YYYY-MM-DD');
+  const [dateFrom, setDateFrom] = useState<string>(INITIAL_DATE);
+  const [dateTo, setDateTo] = useState(END_DATE);
   const ref = useRef<HTMLDivElement>(null);
   const [showCalendar, setShowCalendar] = useState<boolean>(false);
   useOutsideClick([ref], showCalendar, () => setShowCalendar(false));
 
   const [ranges, setRanges] = useState<Range[]>([
     {
-      startDate: moment(INITIAL_DATE).toDate(),
-      endDate: moment().toDate(),
+      startDate: startDate,
+      endDate: endDate,
       key: 'selection',
     },
   ]);
 
   const handleChange = (data: RangeKeyDict) => {
-    setRanges([data.selection]);
-    props.onAcceptedDateRangeChange({
-      accepted_from: moment(data.selection.startDate!).format('yyyy-MM-DD'),
-      accepted_to: moment(data.selection.endDate!).format('yyyy-MM-DD'),
-    });
+    if (data.selection) {
+      setRanges([data.selection]);
+
+      let times: AcceptedDate = {};
+
+      if (!moment(endDate).isSame(moment(data.selection.endDate))) {
+        times['accepted_to'] = moment(data.selection.endDate!).format('YYYY-MM-DD');
+      }
+
+      if (!moment(startDate).isSame(moment(data.selection.startDate))) {
+        times['accepted_from'] = moment(data.selection.startDate!).format('YYYY-MM-DD');
+      }
+
+      updateAcceptedRange(times);
+    }
+  };
+
+  const updateAcceptedRange = (dates: AcceptedDate) => {
+    if (!isEmpty(dates)) {
+      props.onAcceptedDateRangeChange(dates);
+    }
+  };
+
+  const onChangeDate = (e: ChangeEvent<HTMLInputElement>, type: string) => {
+    switch (type) {
+      case 'from':
+        setDateFrom(e.target.value);
+        return;
+      case 'to':
+        setDateTo(e.target.value);
+        return;
+    }
   };
 
   useEffect(() => {
@@ -53,19 +86,66 @@ const AcceptedDateRange = (props: Props) => {
     ]);
   }, [props.acceptedFrom, props.acceptedTo]); /* eslint-disable-line react-hooks/exhaustive-deps */
 
+  useEffect(() => {
+    let times: AcceptedDate = {};
+    if (dateTo !== END_DATE) {
+      times['accepted_to'] = dateTo;
+    }
+    if (dateFrom !== INITIAL_DATE) {
+      times['accepted_from'] = dateFrom;
+    }
+    updateAcceptedRange(times);
+  }, [dateTo, dateFrom]); /* eslint-disable-line react-hooks/exhaustive-deps */
+
   return (
     <>
       <div className={`fw-bold text-uppercase text-primary ${styles.categoryTitle}`}>
         <small>Accepted</small>
       </div>
 
-      <div className="mt-3">
+      <div className="d-flex flex-column d-md-none">
+        <div className="my-2">
+          <label htmlFor="from" className={`form-label text-uppercase text-muted ${styles.label}`}>
+            From:
+          </label>
+          <input
+            type="date"
+            className="form-control form-control-sm rounded-0"
+            min={INITIAL_DATE}
+            max={dateTo}
+            id="from"
+            value={dateFrom}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => onChangeDate(e, 'from')}
+          />
+        </div>
+        <div>
+          <label htmlFor="to" className={`form-label text-uppercase text-muted ${styles.label}`}>
+            To:
+          </label>
+          <input
+            type="date"
+            className="form-control form-control-sm rounded-0"
+            min={dateFrom}
+            max={END_DATE}
+            id="to"
+            value={dateTo}
+            onChange={(date: any) => onChangeDate(date, 'to')}
+          />
+        </div>
+      </div>
+
+      <div className="d-none d-md-block mt-3">
         <button
           className={`btn btn-sm btn-outline-secondary rounded-0 w-100 ${styles.dateBtn}`}
           onClick={() => setShowCalendar(!showCalendar)}
           aria-label="Open calendar"
         >
-          {moment(ranges[0].startDate!).format('MMM D, yyyy')} - {moment(ranges[0].endDate!).format('MMM D, yyyy')}
+          <span className="d-none d-xxl-block">
+            {moment(ranges[0].startDate!).format('MMM D, YYYY')} - {moment(ranges[0].endDate!).format('MMM D, YYYY')}
+          </span>
+          <span className="d-block d-xxl-none">
+            {moment(ranges[0].startDate!).format('MMM D, YY')} - {moment(ranges[0].endDate!).format('MMM D, YY')}
+          </span>
         </button>
 
         <div
@@ -88,8 +168,8 @@ const AcceptedDateRange = (props: Props) => {
             onChange={handleChange}
             moveRangeOnFirstSelection={false}
             ranges={ranges}
-            minDate={moment(INITIAL_DATE).toDate()}
-            maxDate={moment().toDate()}
+            minDate={startDate}
+            maxDate={endDate}
           />
         </div>
       </div>

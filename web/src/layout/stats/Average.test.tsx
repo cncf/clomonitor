fcc3e655a5bb9ca41788e03c7f6d7d4a57245ab1
Cintia Sanchez Garcia <cynthiasg@icloud.com>
2022-06-23 16:29:07 +0200

    Add some unit tests for stats page
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

---
+++ web/src/layout/stats/Average.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react';
+
+import Average from './Average';
+
+const defaultProps = {
+  data: { documentation: 85, license: 79, best_practices: 57, security: 53, legal: 40, global: 69 },
+  title: 'Sandbox',
+};
+
+describe('Average', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('creates snapshot', () => {
+    const { asFragment } = render(<Average {...defaultProps} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe('Render', () => {
+    it('renders component', () => {
+      render(<Average {...defaultProps} />);
+
+      expect(screen.getByText('Sandbox')).toBeInTheDocument();
+      expect(screen.getByText('Documentation')).toBeInTheDocument();
+      expect(screen.getByText('85%')).toBeInTheDocument();
+      expect(screen.getByText('License')).toBeInTheDocument();
+      expect(screen.getByText('79%')).toBeInTheDocument();
+      expect(screen.getByText('Best Practices')).toBeInTheDocument();
+      expect(screen.getByText('57%')).toBeInTheDocument();
+      expect(screen.getByText('Security')).toBeInTheDocument();
+      expect(screen.getByText('53%')).toBeInTheDocument();
+      expect(screen.getByText('Legal')).toBeInTheDocument();
+      expect(screen.getByText('40%')).toBeInTheDocument();
+
+      expect(screen.getAllByRole('progressbar')).toHaveLength(5);
+    });
+  });
+});
---
+++ web/src/layout/stats/Checks.test.tsx
@@ -0,0 +1,68 @@
+import { render, screen } from '@testing-library/react';
+
+import Checks from './Checks';
+
+const defaultProps = {
+  data: {
+    artifacthub_badge: 9,
+    cla: 12,
+    community_meeting: 43,
+    dco: 83,
+    ga4: 8,
+    openssf_badge: 59,
+    recent_release: 81,
+    slack_presence: 33,
+  },
+  title: 'Best Practices',
+};
+
+describe('Checks', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('creates snapshot', () => {
+    const { asFragment } = render(<Checks {...defaultProps} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  describe('Render', () => {
+    it('renders component', () => {
+      render(<Checks {...defaultProps} />);
+
+      expect(screen.getByText('Best Practices')).toBeInTheDocument();
+      expect(screen.getByText('Artifact Hub badge')).toBeInTheDocument();
+      expect(screen.getByText('9%')).toBeInTheDocument();
+      expect(screen.getByText('CLA')).toBeInTheDocument();
+      expect(screen.getByText('12%')).toBeInTheDocument();
+      expect(screen.getByText('Community meeting')).toBeInTheDocument();
+      expect(screen.getByText('43%')).toBeInTheDocument();
+      expect(screen.getByText('DCO')).toBeInTheDocument();
+      expect(screen.getByText('83%')).toBeInTheDocument();
+      expect(screen.getByText('Google Analytics 4')).toBeInTheDocument();
+      expect(screen.getByText('8%')).toBeInTheDocument();
+      expect(screen.getByText('OpenSSF badge')).toBeInTheDocument();
+      expect(screen.getByText('59%')).toBeInTheDocument();
+      expect(screen.getByText('Recent release')).toBeInTheDocument();
+      expect(screen.getByText('81%')).toBeInTheDocument();
+      expect(screen.getByText('Slack presence')).toBeInTheDocument();
+      expect(screen.getByText('33%')).toBeInTheDocument();
+
+      expect(screen.getAllByRole('progressbar')).toHaveLength(8);
+    });
+
+    it('renders checks properly sorted', () => {
+      render(<Checks data={{ approved: 93, scanning: 22, spdx_id: 94 }} title="License" />);
+
+      expect(screen.getAllByText('License')).toHaveLength(2);
+      expect(screen.getByText('Approved license')).toBeInTheDocument();
+      expect(screen.getByText('License scanning')).toBeInTheDocument();
+
+      const progressbar = screen.getAllByRole('progressbar');
+      expect(progressbar[0]).toHaveStyle({ width: '94%' });
+      expect(progressbar[1]).toHaveStyle({ width: '93%' });
+      expect(progressbar[2]).toHaveStyle({ width: '22%' });
+    });
+  });
+});

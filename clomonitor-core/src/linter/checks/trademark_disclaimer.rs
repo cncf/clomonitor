f0fc9883ad72aabaa43dccb45071b35b190c0783
Matthew Pereira <mail@matthewpereira.com>
2022-12-16 14:41:25 -0400

    Update trademark_disclaimer to allow for new URL
    In order to account for The Linux Foundation's new `/legal/trademark-usage` route, this regex on line 20 adds an optional `legal/` capture group.
    
    Signed-off-by: Matthew Pereira <mail@matthewpereira.com>
    Signed-off-by: Administrator <mail@matthewpereira.com>

--- clomonitor-core/src/linter/checks/trademark_disclaimer.rs
+++ clomonitor-core/src/linter/checks/trademark_disclaimer.rs
@@ -19,7 +19,7 @@ pub(crate) const CHECK_SETS: [CheckSet; 1] = [CheckSet::Community];
 lazy_static! {
     #[rustfmt::skip]
     pub(crate) static ref TRADEMARK_DISCLAIMER: RegexSet = RegexSet::new(vec![
-        r"https://(?:w{3}\.)?linuxfoundation.org/trademark-usage",
+        r"https://(?:w{3}\.)?linuxfoundation.org/(?:legal/)?trademark-usage",
         r"The Linux Foundation.* has registered trademarks and uses trademarks",
     ]).expect("exprs in TRADEMARK_DISCLAIMER to be valid");
 }

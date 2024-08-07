f1035ab2c0e969b314636033186cc3f1be017445
Cintia Sánchez García <cynthiasg@icloud.com>
2024-08-07 11:43:53 +0200

    Upgrade frontend dependencies (#1553)
    Signed-off-by: Cintia Sanchez Garcia <cynthiasg@icloud.com>

--- web/src/layout/detail/repositories/index.test.tsx
+++ web/src/layout/detail/repositories/index.test.tsx
@@ -15,7 +15,7 @@ jest.mock('react-router-dom', () => ({
 }));
 
 const getRepositories = (fixtureId: string): Repository[] => {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   return require(`./__fixtures__/index/${fixtureId}.json`) as Repository[];
 };
 

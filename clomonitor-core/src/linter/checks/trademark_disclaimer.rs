80ac0fc47f852fb0091c667e5a8bb3597bfae345
Sergio Castaño Arteaga <tegioz@icloud.com>
2025-07-14 13:15:59 +0200

    Improve trademark disclaimer check
    Signed-off-by: Sergio Castaño Arteaga <tegioz@icloud.com>

--- clomonitor-core/src/linter/checks/trademark_disclaimer.rs
+++ clomonitor-core/src/linter/checks/trademark_disclaimer.rs
@@ -23,6 +23,7 @@ pub(crate) static TRADEMARK_DISCLAIMER: LazyLock<RegexSet> = LazyLock::new(|| {
     RegexSet::new([
         r"https://(?:w{3}\.)?linuxfoundation.org/(?:legal/)?trademark-usage",
         r"The Linux Foundation.* has registered trademarks and uses trademarks",
+        r"Copyright © .+ a Series of LF Projects, LLC",
     ])
     .expect("exprs in TRADEMARK_DISCLAIMER to be valid")
 });
@@ -54,5 +55,8 @@ mod tests {
         assert!(TRADEMARK_DISCLAIMER.is_match(
             "The Linux Foundation® (TLF) has registered trademarks and uses trademarks."
         ));
+        assert!(
+            TRADEMARK_DISCLAIMER.is_match("Copyright © Kubernetes a Series of LF Projects, LLC")
+        );
     }
 }
